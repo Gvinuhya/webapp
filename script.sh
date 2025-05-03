cdd2e5d4f0c4dd3c3ea9589769bb0760f73ed9f2
Gvinuhya <Gvinuhya@gmail.com>
2025-05-03 07:03:34 +0000

    complete web app deploy  script

--- script.sh
+++ script.sh
@@ -1,2 +1,19 @@
-#!bin/bash
+#!/bin/bash
 echo "Deploying Web Apps On NGINX"
+
+echo "updating system"
+sudo apt update -y
+
+echo "install utilities"
+sudo apt install -y zip unzip
+
+echo "install NGINX"
+sudo apt install -y nginx
+
+echo "remove old file"
+sudo rm -r /var/www/html
+
+echo "deploy login app"
+sudo git clone https://github.com/Gvinuhya/webapp.git /var/www/html
+
+echo "Deployed Web Apps On NGINX"
